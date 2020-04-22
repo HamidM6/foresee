58648267877e3b6ba9330defe319977f914820de
Hamid <33704063+HamidM6@users.noreply.github.com>
2020-04-22 09:54:32 -0400

    add dash app

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from distutils.core import setup
 
 setup(name='foresee',
-      version='0.1.0a2',
+      version='0.1.0a3',
       description='Collection of popular forecasting models in python.',
       author='Hamid Mohammadi',
       author_email='hmohammadi6545@gmail.com',
