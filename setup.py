b57f6d4bb9c56622d3787ea874d26a6a6b5506d7
Hamid <33704063+HamidM6@users.noreply.github.com>
2020-05-08 15:11:41 -0400

    update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
 					name='foresee',
-					version='0.1.0a6',
+					version='0.1.0a7',
 					author='Hamid Mohammadi',
 					author_email='hmohammadi6545@gmail.com',
 					description='Generate forecasts using several time series forecasting models in python.',
