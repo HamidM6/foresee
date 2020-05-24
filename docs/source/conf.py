6b5fe75f66e1ca21b7eea82135f0b4d4a297c2a3
Hamid <33704063+HamidM6@users.noreply.github.com>
2020-05-24 18:01:40 -0400

    added copybutton to codeblocks

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -33,7 +33,8 @@ release = '0.1.0a8'
 extensions = [
 					'sphinx.ext.autodoc',
 					'sphinx.ext.viewcode',
-					'sphinx.ext.napoleon'
+					'sphinx.ext.napoleon',
+					'sphinx_copybutton'
 			]
 
 # Add any paths that contain templates here, relative to this directory.
