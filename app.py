8145a81c9a59c882bd09f6a9c5806adb5eca33de
Hamid <33704063+HamidM6@users.noreply.github.com>
2020-05-25 17:19:25 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,47 @@
 # run this command
-import flask
+# import flask
+# import dash
+
+
+# server = flask.Flask(__name__)
+# app = dash.Dash(__name__, server=server)
+
+# # @server.route('/')
+# # def index():
+    # # return 'Flask root.'
+
+# from foresee.webapp.dash_app import app
+
+# if __name__ == '__main__':
+    # app.run_server()
+	
+import os
+
 import dash
+import dash_core_components as dcc
+import dash_html_components as html
 
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
-server = flask.Flask(__name__)
-app = dash.Dash(__name__, server=server)
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-# @server.route('/')
-# def index():
-    # return 'Flask root.'
+server = app.server
 
-from foresee.webapp.dash_app import app
+app.layout = html.Div([
+    html.H2('Hello World'),
+    dcc.Dropdown(
+        id='dropdown',
+        options=[{'label': i, 'value': i} for i in ['LA', 'NYC', 'MTL']],
+        value='LA'
+    ),
+    html.Div(id='display-value')
+])
+
+@app.callback(dash.dependencies.Output('display-value', 'children'),
+              [dash.dependencies.Input('dropdown', 'value')])
+def display_value(value):
+    return 'You have selected "{}"'.format(value)
 
 if __name__ == '__main__':
-    app.run_server()
+    app.run_server(debug=True)
     
