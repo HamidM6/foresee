aa603defd00e35a447f1cbcba6c2f5c957584cf8
Hamid <33704063+HamidM6@users.noreply.github.com>
2020-05-25 10:20:58 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,13 +2,15 @@
 import flask
 import dash
 
-server = flask.Flask(__name__)
-
-@server.route('/')
-def index():
-    return 'Flask root.'
-
 from foresee.webapp.dash_app import app
+
+server = flask.Flask(__name__)
+app = dash.Dash(__name__, server=server)
+
+# @server.route('/')
+# def index():
+    # return 'Flask root.'
+
 
 if __name__ == '__main__':
     app.run_server()
